1f4704f2de9431fa1164919e94ad71416c9c93d7
Diego <morkan@gmail.com>
2021-04-25 12:04:06 -0300

    backend concluido

---
+++ Carteira/backend/src/controllers/UserController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from 'express';
+import EmailService from "../services/EmailService";
+
+const users = [
+    { name: 'Diego', email: 'morkan@gmail.com' },
+];
+
+export default {
+    async index(req: Request, res: Response) {
+        return res.json(users);
+    },
+
+    async create(req: Request, res: Response) {
+        const emailService = new EmailService();
+        
+        emailService.sendMail({
+            to: { 
+                name: 'Diego Andrade', 
+                email: 'morkan@gmail.com' 
+            }, 
+            message: { 
+                subject: 'Bem vindo ao sistema', 
+                body: 'Seja bem vindo' 
+            }
+        });
+
+        return res.send();
+    }
+};
